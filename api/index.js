b881f0ddbe6d4411e0200e127c2bbf5e51a0724f
titok25 <titoooook25@gmail.com>
2026-03-25 15:31:20 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 // --- CONFIGURAÇÕES ---
 // COLOQUE O LINK DA SUA OFERTA REAL (BLACK) ABAIXO:
-const URL_OFERTA_BLACK = "https://link-da-sua-oferta-aqui.com"; 
+const URL_OFERTA_BLACK = "https://mercadolivecomdesconto.com/952367148"; 
 
 // Caminho para a Página Segura (White) local
 const PATH_PAGINA_WHITE = path.join(__dirname, "..", "public", "white.html"); 
